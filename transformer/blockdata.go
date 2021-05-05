04d8f18e776b828730787c8a4280bf5fff905f4b
Hanjun Kim <hallazzang@gmail.com>
2021-05-05 21:57:12 +0900

    remove unused method

--- transformer/blockdata.go
+++ transformer/blockdata.go
@@ -5,8 +5,6 @@ import (
 	liquiditytypes "github.com/tendermint/liquidity/x/liquidity/types"
 	abcitypes "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-
-	"github.com/b-harvest/gravity-dex-backend/schema"
 )
 
 type BlockData struct {
@@ -14,20 +12,6 @@ type BlockData struct {
 	BankModuleState *banktypes.GenesisState `json:"bank_module_states"`
 	Events          []abcitypes.Event       `json:"end_block_events"`
 	Pools           []liquiditytypes.Pool   `json:"pools"`
-}
-
-func (d *BlockData) BalancesByAddress() map[string][]schema.Coin {
-	m := make(map[string][]schema.Coin)
-	if d.BankModuleState != nil {
-		for _, b := range d.BankModuleState.Balances {
-			coins := []schema.Coin{}
-			for _, c := range b.Coins {
-				coins = append(coins, schema.Coin{Denom: c.Denom, Amount: c.Amount.Int64()})
-			}
-			m[b.Address] = coins
-		}
-	}
-	return m
 }
 
 func (d *BlockData) PoolByID() map[uint64]liquiditytypes.Pool {
